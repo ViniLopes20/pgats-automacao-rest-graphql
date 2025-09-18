4deef670bb2e8504702e44145ae34278ca2d6102
ViniLopes20 <vinicius.lopes@amigotech.com.br>
2025-09-17 23:39:28 -0300

    Add notasExternal tests

---
+++ test/REST/external/notasExternal.test.js
@@ -0,0 +1,44 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+require('dotenv').config();
+
+const loginUserBody = require('../fixture/requisicoes/login/loginUser.json');
+let responseLogin;
+
+before(async () => {
+    responseLogin = await request(process.env.BASE_URL_REST)
+        .post('/login')
+        .send(loginUserBody);
+});
+
+describe('/notas - POST API REST Controller', () => {
+    const testesDeErrosDeNotas = require('../fixture/requisicoes/notas/notasWithError.json');
+    testesDeErrosDeNotas.forEach(testes => {
+        it(testes.nomeTeste, async () => {
+            const response = await request(process.env.BASE_URL_REST)
+                .post('/notas')
+                .set({
+                    Authorization: `Bearer ${responseLogin.body.token}`
+                })
+                .send(testes.registerNotasBody);
+
+            expect(response.status).to.equal(testes.status);
+            expect(response.body).to.have.property('message', testes.mensagem);
+        });
+    });
+
+    it('Sucesso deve acontecer ao cadastrar nota válida para aluno existente', async () => {
+        const notasRegisterBody = require('../fixture/requisicoes/notas/registerNotas.json');
+        const response = await request(process.env.BASE_URL_REST)
+            .post('/notas')
+            .set({
+                Authorization: `Bearer ${responseLogin.body.token}`
+            })
+            .send(notasRegisterBody);
+        
+        const usersResponseSuccess = require('../fixture/response/notas/responseWithSuccessToRegister.json');
+        expect(response.status).to.equal(usersResponseSuccess.status);
+        expect(response.body).to.have.property('message', usersResponseSuccess.message);
+    });
+});
